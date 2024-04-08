999b15fdd6a75a782fc259801d28192b13222699
Rafael Meireles <rafameireles2011@gmail.com>
2024-04-08 08:44:04 -0300

    add Mapper

--- src/main/java/br/com/rafaelmeireles/webfluxcourse/mapper/UserMapper.java
+++ src/main/java/br/com/rafaelmeireles/webfluxcourse/mapper/UserMapper.java
@@ -2,12 +2,19 @@ package br.com.rafaelmeireles.webfluxcourse.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.springframework.data.repository.query.parser.Part.IgnoreCaseType;
 
 import br.com.rafaelmeireles.webfluxcourse.entity.User;
 import br.com.rafaelmeireles.webfluxcourse.model.request.UserRequest;
 
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
+import static org.mapstruct.NullValueCheckStrategy.ALWAYS;;
+
 @Mapper(
-    componentModel = "spring"
+    componentModel = "spring",
+    nullValuePropertyMappingStrategy = IGNORE,
+    nullValueCheckStrategy = ALWAYS
 )
 public interface UserMapper {
 
